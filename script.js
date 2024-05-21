a59ab79970c548360767c2d95ecde8041d1888d8
matildetarchi <matilde.tarchi@stud.unifi.it>
2024-05-21 15:56:30 +0200

    last commit2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainNavbar = document.querySelector('.main-navbar');
     const secondaryNav = document.querySelector('.secondary-nav');
     const wrapper = document.querySelector('.buttons-wrapper');
+    const mainElements = document.getElementById('navbarNav');
 
 
     //funzione per cambiare la scritta meteo
@@ -92,6 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
             secondaryNav.style.display = 'flex';
             secondaryNav.style.top = '0';
             mainNavbar.style.top = '50px';
+            mainElements.style.display = 'none';
 
             if (scrollPosition > 50) { // Nascondi la secondaryNav quando si scorre
 
@@ -102,6 +104,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Gestione per schermi grandi
             mainNavbar.classList.remove('nav-shown');
             mainNavbar.classList.remove('nav-shown1');
+            mainElements.style.display = 'fixed';
             mainNavbar.style.position = 'fixed';
             mainNavbar.style.top = '0'; // Posiziona la mainNavbar in cima
             secondaryNav.style.display = 'none'; // Nascondi la secondaryNav
@@ -119,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const showCarousel2 = document.getElementById('showCarousel2');
         const showCarousel3 = document.getElementById('showCarousel3');
 
-        if (window.matchMedia("(max-width: 576px)").matches) {
+        if (window.matchMedia("(max-width: 960px)").matches) {
             showLarge.style.display = 'none';
             showLarge1.style.display = 'none';
             showLarge2.style.display = 'none';
